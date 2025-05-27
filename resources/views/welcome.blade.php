c935d65fb16c95b6302c3190da5e7e72c61c3b24
Noman5713 <noman15-5713@diu.edu.bd>
2025-05-27 17:30:12 +0600

    initial interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,59 @@
     <title>Document</title>
 </head>
 <body>
-    <h1> hello world</h1>
+    <div class="div1">
+        <div>
+            <div>
+                <h3>Abdullah <br>Al<br>Noman
+            </div>
+            <div>
+                <div>
+                    ABOUT ME
+                </div>
+                <div>
+                    EDUCATION
+                </div>
+                <div>
+                    RESEARCH
+                </div>
+                <div>
+                    PUBLICATION
+                </div>
+                <div>
+                    PROJECTS
+                </div>
+                <div>
+                    EXPERIENCE
+                </div>
+                <div>
+                    NEWS
+                </div>
+                <div>
+                    ECA
+                </div>
+                <div>
+                    CONTACT
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+        <div>
+            <div>
+                <h1>ABOUT ME</h1>
+                <div>
+                    <img src="{{asset('assets\images\noman.jpg')}}" alt="error">
+                </<div>
+                <div>
+                    <p>Hello there, I am Abdullah Al Noman.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
